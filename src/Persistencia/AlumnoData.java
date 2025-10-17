f0f5491bfd61fcf51013df8336aec011a57e24eb
gabrielaballerini-sketch <gabrielaballerini@gmail.com>
2025-10-17 10:35:55 -0300

    Formulario , final

--- src/Persistencia/AlumnoData.java
+++ src/Persistencia/AlumnoData.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Persistencia;
 
 import Modelo.Alumno;
@@ -14,10 +10,8 @@ import java.sql.ResultSet;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
-/**
- *
- * @author fatimaalcaraz
- */
+
+
 public class AlumnoData {
     
     private Connection conn= null;
--- src/Persistencia/InscripcionData.java
+++ src/Persistencia/InscripcionData.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Persistencia;
 
 import Modelo.Alumno;
@@ -18,10 +15,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author Brian D
- */
+
 public class InscripcionData {
 
     private Connection con = null;
